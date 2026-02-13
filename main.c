b832e25696ecf31b2c10f522e4dc13a1c9bc9311
Yeats Aiden <ayeats097@gmail.com>
2026-02-13 14:24:21 +0200

    added some consistency

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
 
 
-void print_int_array(int length, int array[length]);
-void bubble_sort(int length, int array[length]);
-void shell_sort(int length, int array[length]);
-void insertion_sort(int length, int array[length]);
-void merge(int array[], int left, int mid, int right);
-void merge_sort(int array[], int left, int right);
-void heap_sort(int arr[], int n);
-void heapify(int arr[], int n, int i);
+void print_int_arr(int *arr, int length);
+void bubble_sort(int *arr, int length);
+void shell_sort(int *arr, int length);
+void insertion_sort(int *arr, int length);
+void merge(int *arr, int left, int mid, int right);
+void merge_sort(int *arr, int left, int right);
+void heap_sort(int *arr, int n);
+void heapify(int *arr, int n, int i);
 void selection_sort(int *arr, int n);
 
 
@@ -31,69 +30,69 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    print_int_array(len, sort_me);
+    print_int_arr(sort_me, len);
     printf("\n");
 
-    bubble_sort(len, sort_me);
-    print_int_array(len, sort_me);
+    bubble_sort(sort_me, len);
+    print_int_arr(sort_me, len);
     printf("\n");
 
-    shell_sort(len, sort_me);
-    print_int_array(len, sort_me);
+    shell_sort(sort_me, len);
+    print_int_arr(sort_me, len);
     printf("\n");
     
-    insertion_sort(len, sort_me);
-    print_int_array(len, sort_me);
+    insertion_sort(sort_me, len);
+    print_int_arr(sort_me, len);
     printf("\n");
 
     merge_sort(sort_me, 0, len - 1);
-    print_int_array(len, sort_me);
+    print_int_arr(sort_me, len);
     printf("\n");
 }
 
 
-void bubble_sort(int length, int array[length]) {
+void bubble_sort(int *arr, int length) {
     uint8_t sorted = 0;
     while (!sorted) {
         sorted = 1;
         for(int i = 1; i < length; i++) {
-            if(array[i-1] > array[i]) {
-                int temp = array[i];
-                array[i] = array[i-1];
-                array[i-1] = temp;
+            if(arr[i-1] > arr[i]) {
+                int temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
                 sorted = 0;
             }
         }
     }
 }
 
-void shell_sort(int length, int array[length]) {
+void shell_sort(int *arr, int length) {
     for (int gap = length / 2; gap > 0; gap /= 2) {
         for (int i = gap; i < length; i++) {
-            int temp = array[i];
+            int temp = arr[i];
             int j = i;
-            while (j >= gap && array[j - gap] > temp) {
-                array[j] = array[j - gap];
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
                 j -= gap;
             }
-            array[j] = temp;
+            arr[j] = temp;
         }
     }
 }
 
-void insertion_sort(int length, int array[length]) {
+void insertion_sort(int *arr, int length) {
     for (int i = 1 ; i < length - 1 ; i++) {
-        int key = array[i];
+        int key = arr[i];
         int j = i - 1 ;
-        while (j >= 0 && array[j] > key) {
-            array[j+1] = array[j];
+        while (j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
             j = j - 1;
         }
-        array[j+1] = key;
+        arr[j+1] = key;
     }
 }
 
-void merge(int array[], int left, int mid, int right) {
+void merge(int *arr, int left, int mid, int right) {
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -101,57 +100,57 @@ void merge(int array[], int left, int mid, int right) {
     int left_arr[n1], right_arr[n2];
 
     for (i = 0; i < n1; i++)
-        left_arr[i] = array[left + i];
+        left_arr[i] = arr[left + i];
     for (j = 0; j < n2; j++)
-        right_arr[j] = array[mid + 1 + j];
+        right_arr[j] = arr[mid + 1 + j];
 
     i = 0;
     j = 0;
     k = left;
     while (i < n1 && j < n2) {
         if (left_arr[i] <= right_arr[j]) {
-            array[k] = left_arr[i];
+            arr[k] = left_arr[i];
             i++;
         }
         else {
-            array[k] = right_arr[j];
+            arr[k] = right_arr[j];
             j++;
         }
         k++;
     }
 
     while (i < n1) {
-        array[k] = left_arr[i];
+        arr[k] = left_arr[i];
         i++;
         k++;
     }
 
     while (j < n2) {
-        array[k] = right_arr[j];
+        arr[k] = right_arr[j];
         j++;
         k++;
     }
 }
 
-void merge_sort(int array[], int left, int right) {
+void merge_sort(int *arr, int left, int right) {
     if (left < right) {
-      
+
         int mid = left + (right - left) / 2;
 
-        merge_sort(array, left, mid);
-        merge_sort(array, mid + 1, right);
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
 
-        merge(array, left, mid, right);
+        merge(arr, left, mid, right);
     }
 }
 
-void heap_sort(int arr[], int n)
+void heap_sort(int *arr, int length)
 {
-    for (int i = n/2; i >= 0; i--)
+    for (int i = length/2; i >= 0; i--)
     {
-        heapify(arr, n, i);
+        heapify(arr, length, i);
     }
-    for (int i = n - 1; i > 0; i--)
+    for (int i = length - 1; i > 0; i--)
     {
         int tmp = arr[0];
         arr[0] = arr[i];
@@ -160,17 +159,17 @@ void heap_sort(int arr[], int n)
     }
 }
 
-void heapify(int arr[], int n, int i)
+void heapify(int *arr, int length, int i)
 {
     int max = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
     
-    if (left < n && arr[left] > arr[max])
+    if (left < length && arr[left] > arr[max])
     {
         max = left;
     }
-    if (right < n && arr[right] > arr[max])
+    if (right < length && arr[right] > arr[max])
     {
         max = right;
     }
@@ -179,7 +178,7 @@ void heapify(int arr[], int n, int i)
         int tmp = arr[i];
         arr[i] = arr[max];
         arr[max] = tmp;
-        heapify(arr, n, max);
+        heapify(arr, length, max);
     }
 }
 
@@ -204,10 +203,10 @@ void selection_sort(int *arr, int n) {
 	}
 }
 
-void print_int_array(int length, int array[length]) {
+void print_int_arr(int *arr, int length) {
     printf("{");
     for (int i = 0; i < length; i++) {
-        printf("%d", array[i]);
+        printf("%d", arr[i]);
 
         if (i != length-1) {
             printf(", ");
