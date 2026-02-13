934e97cb4ccad3ddcadd7404b67b0a4b555eef57
Admin <max.12.viv@gmail.com>
2026-02-13 14:02:44 +0200

    Added heap sort

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ void shell_sort(int length, int array[length]);
 void insertion_sort(int length, int array[length]);
 void merge(int array[], int left, int mid, int right);
 void merge_sort(int array[], int left, int right);
+void heap_sort(int arr[], int n);
+void heapify(int arr[], int n, int i);
 
 
 int main(int argc, char *argv[]) {
@@ -142,7 +144,43 @@ void merge_sort(int array[], int left, int right) {
     }
 }
 
+void heap_sort(int arr[], int n)
+{
+    for (int i = n/2; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    for (int i = n - 1; i > 0; i--)
+    {
+        int tmp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = tmp;
+        heapify(arr, i, 0);
+    }
+}
 
+void heapify(int arr[], int n, int i)
+{
+    int max = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+    
+    if (left < n && arr[left] > arr[max])
+    {
+        max = left;
+    }
+    if (right < n && arr[right] > arr[max])
+    {
+        max = right;
+    }
+    if (max != i)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[max];
+        arr[max] = tmp;
+        heapify(arr, n, max);
+    }
+}
 
 void print_int_array(int length, int array[length]) {
     printf("{");
