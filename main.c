24091135bbf3e6256b88a5290c1fd160f73c464c
Admin <max.12.viv@gmail.com>
2026-02-16 16:12:54 +0200

    added timsort

--- main.c
+++ main.c
@@ -4,17 +4,24 @@
 #include <stdint.h>
 #include <string.h>
 
-
-void print_int_arr(int *arr, int length);
+void selection_sort(int *arr, int length);
+void insertion_sort(int *arr, int length);
 void bubble_sort(int *arr, int length);
 void shell_sort(int *arr, int length);
-void insertion_sort(int *arr, int length);
-void merge(int *arr, int left, int mid, int right);
+// Merge sort
 void merge_sort(int *arr, int left, int right);
-void heap_sort(int *arr, int n);
-void heapify(int *arr, int n, int i);
-void selection_sort(int *arr, int n);
+void merge(int *arr, int left, int mid, int right);
+// Heap sort
+void heap_sort(int *arr, int lenght);
+void heapify(int *arr, int length, int current);
+// Timsort
+void tim_sort(int *arr, int length);
+int find_run(int *arr, int start, int length);
+void insert_for_tim(int *arr, int left, int right);
+// Utility
 void benchmark(char* option);
+void print_int_arr(int *arr, int length);
+void reverse(int *arr, int start, int end);
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
@@ -24,10 +31,12 @@ int main(int argc, char *argv[]) {
         "bubble\0",
         "shell\0",
         "merge\0",
-        "heap\0"
+        "heap\0",
+        "tim\0",
         //add if new sorts
     };
-    for (int i = 0; i < 6-1; i++) //increase if new sorts
+    int options_len = sizeof(options) / sizeof(options[0]);
+    for (int i = 0; i < options_len; i++) 
     {
         benchmark(options[i]);
     }
@@ -36,7 +45,7 @@ int main(int argc, char *argv[]) {
 void benchmark(char* option)
 {
     printf("Started %s sort.\n", option);
-    int size = 10000;
+    int size = 100000;
     int *arr = malloc(size * sizeof(int));
     for (int i = 0; i < size; i++)
     {
@@ -67,6 +76,10 @@ void benchmark(char* option)
     else if(strcmp("selection", option) == 0)
     {
         selection_sort(arr, size);
+    }
+    else if(strcmp("tim", option) == 0)
+    {
+        tim_sort(arr, size);
     }
     //add if new sorts
     clock_t end = clock();
@@ -251,5 +264,111 @@ void print_int_arr(int *arr, int length) {
             printf(", ");
         }
     }
-    printf("}");
+    printf("}\n");
+}
+
+int find_run(int* arr, int start, int length)
+{
+    int end = start + 1;
+    if (end == length)
+    {
+        return end;
+    }
+    if (arr[end] < arr[start])
+    {
+        while (end < length && arr[end] < arr[end-1])
+        {
+            end++;
+        }
+        reverse(arr, start, end);
+    }
+    else
+    {
+        while (end < length && arr[end] >= arr[end-1])
+        {
+            end++;
+        }
+    }
+    return end;
+}
+
+void insert_for_tim(int *arr, int start, int end)
+{
+    for (int i = start + 1 ; i <= end; i++) 
+    {
+        int key = arr[i];
+        int j = i - 1 ;
+        while (j >= start && arr[j] > key) 
+        {
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = key;
+    }
+}
+
+void tim_sort(int *arr, int length)
+{
+    int min_run = 32;
+    int **runs = NULL;
+    int runs_len = 0;
+
+    int i = 0;
+    while (i < length)
+    {
+        int run_end = find_run(arr, i, length);
+        int run_len = run_end - i;
+        if (run_len < min_run)
+        {
+            int end = (i + min_run < length) ? i + min_run : length;
+            insert_for_tim(arr, i, end - 1);
+            run_end = end;
+        }
+        runs = realloc(runs, ++runs_len * sizeof(int*));
+        runs[runs_len-1] = malloc(2 * sizeof(int));
+        runs[runs_len-1][0] = i;
+        runs[runs_len-1][1] = run_end;
+        i = run_end;
+
+        while (runs_len > 1)
+        {
+            int l1 = runs[runs_len - 2][0];
+            int r1 = runs[runs_len - 2][1];
+            int l2 = runs[runs_len - 1][0];
+            int r2 = runs[runs_len - 1][1];
+
+            int len1 = r1 - l1;
+            int len2 = r2 - l2;
+
+            if (len1 <= len2)
+            {
+                merge(arr, l1, r1 - 1, r2 - 1);
+                runs = realloc(runs, (--runs_len) * sizeof(int*));
+                runs[runs_len - 1][0] = l1;
+                runs[runs_len - 1][1] = r2;
+            }
+            else break;
+        }
+    }
+    while (runs_len > 1)
+    {
+        int l1 = runs[runs_len - 2][0];
+        int r1 = runs[runs_len - 2][1];
+        int l2 = runs[runs_len - 1][0];
+        int r2 = runs[runs_len - 1][1];
+        merge(arr, l1, r1 - 1, r2 - 1);
+        runs = realloc(runs, (--runs_len) * sizeof(int*));
+        runs[runs_len - 1][0] = l1;
+        runs[runs_len - 1][1] = r2;
+    }
+}
+
+void reverse(int *arr, int start, int end)
+{
+    for (int i = 0; i < (start + end)/2 - start; i++)
+    {
+        int tmp = arr[start + i];
+        arr[start + i] = arr[end - i];
+        arr[end - i] = tmp;
+    }
 }
