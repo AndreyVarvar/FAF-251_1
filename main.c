e48be60fb6d559892bc5d3aeb26dc2a7d9f47f28
Admin <max.12.viv@gmail.com>
2026-02-17 09:27:43 +0200

    added structure, moved merge on dinamic memory, improved tim sort

--- main.c
+++ main.c
@@ -5,19 +5,19 @@
 #include <string.h>
 
 void selection_sort(int *arr, int length);
-void insertion_sort(int *arr, int length);
+void insertion_sort(int *arr, int start, int end);
 void bubble_sort(int *arr, int length);
 void shell_sort(int *arr, int length);
 // Merge sort
 void merge_sort(int *arr, int left, int right);
-void merge(int *arr, int left, int mid, int right);
+void merge_sort_rec(int *arr, int *tmp, int left, int right);
+void merge(int *arr, int *tmp, int left, int mid, int right);
 // Heap sort
 void heap_sort(int *arr, int lenght);
 void heapify(int *arr, int length, int current);
 // Timsort
 void tim_sort(int *arr, int length);
 int find_run(int *arr, int start, int length);
-void insert_for_tim(int *arr, int left, int right);
 // Utility
 void benchmark(char* option);
 void print_int_arr(int *arr, int length);
@@ -42,65 +42,35 @@ int main(int argc, char *argv[]) {
     }
 }
 
-void benchmark(char* option)
-{
-    printf("Started %s sort.\n", option);
-    int size = 100000;
-    int *arr = malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++)
+void selection_sort(int *arr, int length) {
+	int i, j, min_idx, temp;
+	for (i = 0; i < length - 1; i++) {
+		min_idx = i;
+		for (j = i + 1; j < length; j++) {
+			if (arr[j] < arr[min_idx]) {
+				min_idx = j;
+			}
+		}
+		if (min_idx != i) {
+			temp = arr[i];
+			arr[i] = arr[min_idx];
+			arr[min_idx] = temp;
+		}
+	}
+}
+
+void insertion_sort(int *arr, int start, int end) {
+    for (int i = start + 1 ; i <= end; i++) 
     {
-        int sign = (rand() % 2 == 1) ? 1 : -1;
-        arr[i] = (sign) * rand();
-    }
-    clock_t start = clock();
-    if      (strcmp("bubble", option) == 0)
-    {
-        bubble_sort(arr, size);
-    }
-    else if (strcmp("shell", option) == 0)
-    {
-        shell_sort(arr, size);
-    }
-    else if (strcmp("insertion", option) == 0)
-    {
-        insertion_sort(arr, size);
-    }
-    else if (strcmp("merge", option) == 0)
-    {
-        merge_sort(arr, 0, size-1);
-    }
-    else if (strcmp("heap", option) == 0)
-    {
-        heap_sort(arr, size);
-    }
-    else if(strcmp("selection", option) == 0)
-    {
-        selection_sort(arr, size);
-    }
-    else if(strcmp("tim", option) == 0)
-    {
-        tim_sort(arr, size);
-    }
-    //add if new sorts
-    clock_t end = clock();
-    int check = 1;
-    for (int i = 1; i < size; i++)
-    {
-        if (arr[i-1] > arr[i])
+        int key = arr[i];
+        int j = i - 1 ;
+        while (j >= start && arr[j] > key) 
         {
-            check = 0;
-            break;
+            arr[j+1] = arr[j];
+            j = j - 1;
         }
+        arr[j+1] = key;
     }
-    if (check)
-    {
-        printf("%s sort successfull. CPU time: %d\n", option, end-start);
-    }
-    else
-    {
-        printf("%s sort unsuccessfull.\n", option);
-    }
-    free(arr);
 }
 
 void bubble_sort(int *arr, int length) {
@@ -137,68 +107,47 @@ void shell_sort(int *arr, int length) {
         }
     }
 }
+void merge_sort(int *arr, int left, int right)
+{
+    int *tmp = malloc((right - left + 1) * sizeof(int));
+    merge_sort_rec(arr, tmp, left, right);
+    free(tmp);
+}
 
-void insertion_sort(int *arr, int length) {
-    for (int i = 1 ; i < length; i++) {
-        int key = arr[i];
-        int j = i - 1 ;
-        while (j >= 0 && arr[j] > key) {
-            arr[j+1] = arr[j];
-            j = j - 1;
-        }
-        arr[j+1] = key;
+void merge_sort_rec(int *arr, int *tmp, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        merge_sort_rec(arr, tmp, left, mid);
+        merge_sort_rec(arr, tmp, mid + 1, right);
+
+        merge(arr, tmp, left, mid, right);
     }
 }
 
-void merge(int *arr, int left, int mid, int right) {
-    int i, j, k;
+void merge(int *arr, int *tmp, int left, int mid, int right) {
+    int i = left;
+    int j = mid + 1;
+    int k = left;
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    int left_arr[n1], right_arr[n2];
+    for (int x = left; x <= right; x++)
+    {
+        tmp[x] = arr[x];
+    }
 
-    for (i = 0; i < n1; i++)
-        left_arr[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        right_arr[j] = arr[mid + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-        if (left_arr[i] <= right_arr[j]) {
-            arr[k] = left_arr[i];
-            i++;
+    while (i <= mid && j <= right) {
+        if (tmp[i] <= tmp[j]) {
+            arr[k++] = tmp[i++];
         }
         else {
-            arr[k] = right_arr[j];
-            j++;
+            arr[k++] = tmp[j++];
         }
-        k++;
     }
 
-    while (i < n1) {
-        arr[k] = left_arr[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = right_arr[j];
-        j++;
-        k++;
-    }
-}
-
-void merge_sort(int *arr, int left, int right) {
-    if (left < right) {
-
-        int mid = left + (right - left) / 2;
-
-        merge_sort(arr, left, mid);
-        merge_sort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+    while (i <= mid) {
+        arr[k++] = tmp[i++];
     }
 }
 
@@ -240,37 +189,61 @@ void heapify(int *arr, int length, int i)
     }
 }
 
-void selection_sort(int *arr, int n) {
-	int i, j, min_idx, temp;
+void tim_sort(int *arr, int length)
+{
+    int min_run = 32;
+    int *tmp = malloc(length * sizeof(int));
+    int runs[128][2];
+    int runs_len = 0;
 
-	for (i = 0; i < n - 1; i++) {
-		// Find the index of the minimum element in the unsorted portion
-		min_idx = i;
-		for (j = i + 1; j < n; j++) {
-			if (arr[j] < arr[min_idx]) {
-				min_idx = j;
-			}
-		}
+    int i = 0;
+    while (i < length)
+    {
+        int run_end = find_run(arr, i, length);
+        int run_len = run_end - i;
+        if (run_len < min_run)
+        {
+            int end = (i + min_run < length) ? i + min_run : length;
+            insertion_sort(arr, i, end - 1);
+            run_end = end;
+        }
+        runs_len++;
+        runs[runs_len-1][0] = i;
+        runs[runs_len-1][1] = run_end;
+        i = run_end;
 
-		// Swap the found minimum element with the first element of the unsorted portion
-		if (min_idx != i) {
-			temp = arr[i];
-			arr[i] = arr[min_idx];
-			arr[min_idx] = temp;
-		}
-	}
-}
+        while (runs_len > 1)
+        {
+            int l1 = runs[runs_len - 2][0];
+            int r1 = runs[runs_len - 2][1];
+            int l2 = runs[runs_len - 1][0];
+            int r2 = runs[runs_len - 1][1];
 
-void print_int_arr(int *arr, int length) {
-    printf("{");
-    for (int i = 0; i < length; i++) {
-        printf("%d", arr[i]);
+            int len1 = r1 - l1;
+            int len2 = r2 - l2;
 
-        if (i != length-1) {
-            printf(", ");
+            if (len1 <= len2)
+            {
+                merge(arr, tmp, l1, r1 - 1, r2 - 1);
+                runs_len--;
+                runs[runs_len - 1][0] = l1;
+                runs[runs_len - 1][1] = r2;
+            }
+            else break;
         }
     }
-    printf("}\n");
+    while (runs_len > 1)
+    {
+        int l1 = runs[runs_len - 2][0];
+        int r1 = runs[runs_len - 2][1];
+        int l2 = runs[runs_len - 1][0];
+        int r2 = runs[runs_len - 1][1];
+        merge(arr, tmp, l1, r1 - 1, r2 - 1);
+        runs_len--;
+        runs[runs_len - 1][0] = l1;
+        runs[runs_len - 1][1] = r2;
+    }
+    free(tmp);
 }
 
 int find_run(int* arr, int start, int length)
@@ -298,75 +271,77 @@ int find_run(int* arr, int start, int length)
     return end;
 }
 
-void insert_for_tim(int *arr, int start, int end)
+void benchmark(char* option)
 {
-    for (int i = start + 1 ; i <= end; i++) 
+    printf("Started %s sort.\n", option);
+    int size = 100000000;
+    int *arr = malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
     {
-        int key = arr[i];
-        int j = i - 1 ;
-        while (j >= start && arr[j] > key) 
-        {
-            arr[j+1] = arr[j];
-            j = j - 1;
-        }
-        arr[j+1] = key;
+        int sign = (rand() % 2 == 1) ? 1 : -1;
+        arr[i] = (sign) * rand();
     }
+    clock_t start = clock();
+    if      (strcmp("selection", option) == 0)
+    {
+        selection_sort(arr, size);
+    }
+    else if (strcmp("insertion", option) == 0)
+    {
+        insertion_sort(arr, 0, size);
+    }
+    else if (strcmp("bubble", option) == 0)
+    {
+        bubble_sort(arr, size);
+    }
+    else if (strcmp("shell", option) == 0)
+    {
+        shell_sort(arr, size);
+    }
+    else if (strcmp("merge", option) == 0)
+    {
+        merge_sort(arr, 0, size-1);
+    }
+    else if (strcmp("heap", option) == 0)
+    {
+        heap_sort(arr, size);
+    }
+    else if(strcmp("tim", option) == 0)
+    {
+        tim_sort(arr, size);
+    }
+    //add if new sorts
+    clock_t end = clock();
+    int check = 1;
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i-1] > arr[i])
+        {
+            check = 0;
+            break;
+        }
+    }
+    if (check)
+    {
+        printf("%s sort successfull. CPU time: %d\n", option, end-start);
+    }
+    else
+    {
+        printf("%s sort unsuccessfull.\n", option);
+    }
+    free(arr);
 }
 
-void tim_sort(int *arr, int length)
-{
-    int min_run = 32;
-    int **runs = NULL;
-    int runs_len = 0;
+void print_int_arr(int *arr, int length) {
+    printf("{");
+    for (int i = 0; i < length; i++) {
+        printf("%d", arr[i]);
 
-    int i = 0;
-    while (i < length)
-    {
-        int run_end = find_run(arr, i, length);
-        int run_len = run_end - i;
-        if (run_len < min_run)
-        {
-            int end = (i + min_run < length) ? i + min_run : length;
-            insert_for_tim(arr, i, end - 1);
-            run_end = end;
-        }
-        runs = realloc(runs, ++runs_len * sizeof(int*));
-        runs[runs_len-1] = malloc(2 * sizeof(int));
-        runs[runs_len-1][0] = i;
-        runs[runs_len-1][1] = run_end;
-        i = run_end;
-
-        while (runs_len > 1)
-        {
-            int l1 = runs[runs_len - 2][0];
-            int r1 = runs[runs_len - 2][1];
-            int l2 = runs[runs_len - 1][0];
-            int r2 = runs[runs_len - 1][1];
-
-            int len1 = r1 - l1;
-            int len2 = r2 - l2;
-
-            if (len1 <= len2)
-            {
-                merge(arr, l1, r1 - 1, r2 - 1);
-                runs = realloc(runs, (--runs_len) * sizeof(int*));
-                runs[runs_len - 1][0] = l1;
-                runs[runs_len - 1][1] = r2;
-            }
-            else break;
+        if (i != length-1) {
+            printf(", ");
         }
     }
-    while (runs_len > 1)
-    {
-        int l1 = runs[runs_len - 2][0];
-        int r1 = runs[runs_len - 2][1];
-        int l2 = runs[runs_len - 1][0];
-        int r2 = runs[runs_len - 1][1];
-        merge(arr, l1, r1 - 1, r2 - 1);
-        runs = realloc(runs, (--runs_len) * sizeof(int*));
-        runs[runs_len - 1][0] = l1;
-        runs[runs_len - 1][1] = r2;
-    }
+    printf("}\n");
 }
 
 void reverse(int *arr, int start, int end)
